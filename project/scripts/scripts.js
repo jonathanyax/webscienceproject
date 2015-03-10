5ff29802791b3029c7e068d6f52f05f1b348c3af
Devin Nguyen <nguyed5@rpi.edu>
2015-03-10 18:36:57 -0400

    Add image grid to front page

---
+++ project/scripts/scripts.js
@@ -0,0 +1,12 @@
+$(function() {
+	$( '#ri-grid' ).gridrotator( {
+		rows		: 3,
+		columns		: 15,
+		animType	: 'fadeInOut',
+		animSpeed	: 500,
+		interval	: 1500,
+		step		: 1,
+		preventClick: false
+	} );
+
+});
