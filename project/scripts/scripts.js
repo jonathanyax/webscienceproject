ae852640aa700f7bf60d6611a9434fc471c195e3
Emily Roth <errskipower@gmail.com>
2015-03-11 01:51:32 -0400

    added lane-narrow and on-point fonts, updated sign-in button style, created regions page, and moved css to file

--- project/scripts/scripts.js
+++ project/scripts/scripts.js
@@ -10,3 +10,21 @@ $(function() {
 	} );
 
 });
+
+$(document).ready(function() {
+  $('#regions_nav .city').each(function(index, el) {
+    if ($(this).children('a').hasClass('active')) {
+      var showRegion = $(this).children('a').data('show-region');
+      $('#' + showRegion).show();
+    }
+  });
+
+  $('#regions_nav a').on('click', function() {
+    var hideRegion = $('#regions_nav a.active').data('show-region');
+    $('#' + hideRegion).hide();
+    $('#regions_nav a.active').removeClass('active');
+    $(this).addClass('active');
+    var showRegion = $(this).data('show-region');
+    $('#' + showRegion).show();
+  });
+});
