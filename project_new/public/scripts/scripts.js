6eb29d6197007fda1dea27bc1b42b9b5d0ecdf91
Emily Roth <errskipower@gmail.com>
2015-03-15 21:38:04 -0400

    changes to regions nav and search/point pages

--- project_new/public/scripts/scripts.js
+++ project_new/public/scripts/scripts.js
@@ -12,20 +12,71 @@ $(function() {
 });
 
 $(document).ready(function() {
-  $('#regions_nav .city').each(function(index, el) {
-    if ($(this).hasClass('active')) {
-      var showRegion = $(this).children('a').data('show-region');
-      $('#' + showRegion).show();
-    }
-  });
 
-  $('#regions_nav a').on('click', function() {
-    var hideRegion = $('#regions_nav .city.active').children('a').data('show-region');
-    console.log($('#regions_nav .city.active'));
-    $('#' + hideRegion).hide();
-    $('#regions_nav .city.active').removeClass('active');
-    $(this).parent('.city').addClass('active');
-    var showRegion = $(this).data('show-region');
-    $('#' + showRegion).show();
-  });
+	// highlight main navigation
+	var url = (window.location.href).split("/").pop();
+	$('nav ul#functions a[href="/' + url + '"]').addClass('current');
+
+	// show cities corresponding to regions on regions page
+	$('#regions_nav .city').each(function(index, el) {
+		if ($(this).hasClass('active')) {
+			var showRegion = $(this).children('a').data('show-region');
+			$('#' + showRegion).show();
+		}
+	});
+	$('#regions_nav a').on('click', function() {
+		var hideRegion = $('#regions_nav .city.active').children('a').data('show-region');
+		$('#' + hideRegion).hide();
+		$('#regions_nav .city.active').removeClass('active');
+		$(this).parent('.city').addClass('active');
+		var showRegion = $(this).data('show-region');
+		$('#' + showRegion).show();
+	});
+
+	// magic line under regions nav items on regions page
+	if ($('#regions_nav').length) {
+		var $el, leftPos, newWidth,
+				$mainNav = $('#regions_nav');
+
+		$mainNav.append('<li id="magic_line" class="three columns"></li>');
+
+		var $magicLine = $('#magic_line');
+		$magicLine
+			.width($('#regions_nav .active').width())
+			.css('left', $('#regions_nav .active a').position().left)
+			.data('origLeft', $magicLine.position().left)
+			.data('origWidth', $magicLine.width());
+
+		$('#regions_nav li a').hover(function() {
+			$el = $(this);
+			leftPos = $el.position().left;
+			newWidth = $el.parent().width();
+			$magicLine.stop().animate({
+				left: leftPos,
+				width: newWidth
+			});
+		}, function() {
+			$magicLine.stop().animate({
+				left: $magicLine.data('origLeft'),
+				width: $magicLine.data('origWidth')
+			});
+		});
+
+		$('#regions_nav li a').click(function() {
+			$el = $(this);
+			leftPos = $el.position().left;
+			newWidth = $el.parent().width();
+			$magicLine.stop().animate({
+				left: leftPos,
+				width: newWidth
+			});
+		}, function() {
+			$magicLine
+				.data('origLeft', leftPos)
+				.data('origWidth', newWidth);
+		});
+	}
+	
+
+
 });
--- project_new/routes/index.js
+++ project_new/routes/index.js
@@ -11,4 +11,15 @@ router.get('/regions', function(req, res) {
   res.render('regions', {title: 'Regions'})
 })
 
+// define the search route
+router.get('/search', function(req, res) {
+  res.render('search', {title: 'Search'})
+})
+
+// define the search route
+router.get('/point', function(req, res) {
+  res.render('point', {title: 'Point'})
+})
+
+
 module.exports = router
