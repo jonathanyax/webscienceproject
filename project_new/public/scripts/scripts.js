2204b14e0665364173e95a99f893bb1438498051
Devin Nguyen <nguyed5@rpi.edu>
2015-03-15 21:47:00 -0400

    Updating max-width to 1000px and adding points-grid js updates

--- project_new/public/scripts/scripts.js
+++ project_new/public/scripts/scripts.js
@@ -1,14 +1,59 @@
 $(function() {
 	$( '#ri-grid' ).gridrotator( {
 		rows		: 3,
-		columns		: 15,
+		columns		: 12,
 		animType	: 'fadeInOut',
 		animSpeed	: 500,
 		interval	: 2000,
 		step		: 1,
-		preventClick: false
+		preventClick: false,
+		w320		: {
+			rows	: 2,
+			columns	: 2
+		},
+		w480		: {
+			rows	: 2,
+			columns	: 4
+		},
+		w768		: {
+			rows	: 3,
+			columns	: 5
+		},
+		w1024		: {
+			rows	: 3,
+			columns	: 9
+		}
 	} );
-
+	$( '.point-grid' ).gridrotator( {
+		rows		: 2,
+		columns		: 2,
+		animType	: 'fadeInOut',
+		animSpeed	: 500,
+		interval	: 3000,
+		step		: 1,
+		preventClick: false,
+		w240		: {
+			rows	: 3,
+			columns	: 2
+		},
+		w320		: {
+			rows	: 4,
+			columns	: 2
+		},
+		w480		: {
+			rows	: 4,
+			columns	: 3
+		},
+		w768		: {
+			rows	: 4,
+			columns	: 3
+		},
+		w1024		: {
+			rows	: 4,
+			columns	: 3
+		}
+	} );
+	
 });
 
 $(document).ready(function() {
