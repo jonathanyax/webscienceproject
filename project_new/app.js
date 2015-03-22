ae078e92af54fb128d4f1a8cd858e0b4222aa276
Devin Nguyen <nguyed5@rpi.edu>
2015-03-22 01:32:21 -0400

    Updates on search page design and styles

--- project_new/app.js
+++ project_new/app.js
@@ -17,6 +17,7 @@ function compile(str, path) {
 }
 // use Jade and get views from '/views' directory
 app.set('views', __dirname + '/views')
+app.set('port', (process.env.PORT || 5000))
 app.set('view engine', 'jade')
 // middleware to compile .styl files to CSS
 app.use(stylus.middleware(
@@ -30,6 +31,10 @@ app.use(express.static(__dirname + '/public'))
 // run!
 app.use('/', routes)
 
-app.listen(3000, function() {
-	console.log('Server up on port 3000')
+// app.listen(3000, function() {
+// 	console.log('Server up on port 3000')
+// })
+
+app.listen(app.get('port'), function() {
+  console.log("OnPoint is running on:" + app.get('port'))
 })
