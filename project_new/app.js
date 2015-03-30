10094410cb77ba8b0d743c6477dfd228a4cb0e6e
Devin Nguyen <nguyed5@rpi.edu>
2015-03-29 22:06:39 -0400

    Explorations on mongo

--- project_new/app.js
+++ project_new/app.js
@@ -6,25 +6,37 @@ var express = require('express'),
 		socket  = require('socket.io'),
 		stylus  = require('stylus'),
 		nib     = require('nib'),
-		routes  = require('./routes')
+		routes  = require('./routes'),
+		mongoose = require('mongoose')
 
 // set up express app
 var app = express()
+
 function compile(str, path) {
 	return stylus(str)
 		.set('filename', path)
 		.use(nib())
 }
+
+// set default node environment variables to development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+// connect to mongo db
+var db = mongoose.connection;
+db.on('error', console.error);
+
 // use Jade and get views from '/views' directory
 app.set('views', __dirname + '/views')
 app.set('port', (process.env.PORT || 5000))
 app.set('view engine', 'jade')
+
 // middleware to compile .styl files to CSS
 app.use(stylus.middleware(
 	{ src: __dirname + '/public',
 	  compile: compile
 	}
 ))
+
 // static files in '/public' directory
 app.use(express.static(__dirname + '/public'))
 
