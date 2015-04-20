5089973ba1dc5a1c45b99bb7977e93b1b1c8de54
Devin Nguyen <nguyed5@rpi.edu>
2015-04-20 15:54:09 -0400

    merging disaster 2k15

---
+++ passport_sandbox/passport/routes/index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var Account = require('../models/account');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { user : req.user });
+});
+
+router.get('/register', function(req, res) {
+	res.render ('register', {});
+});
+
+router.post('/register', function(req, res) {
+	Account.register(new Account({username: req.body.username}), req.body.password, function (err, account) {
+		if (err) return res.render('register', {account:account});
+		passport.authenticate('local')(req, res, function() {
+			res.redirect('/');
+		});
+	});
+});
+
+router.get('/login', function(req, res) {
+	res.render('login', {user:req.user});
+});
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+	res.redirect('/');
+});
+
+router.get('/logout', function(req, res) {
+	req.logout();
+	res.redirect('/');
+});
+
+// testing, will delete
+router.get('/ping', function(req, res) {
+	res.status(200).send("pong!");
+});
+
+module.exports = router;
