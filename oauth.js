00a6a9e02c7f8ccc89180d9efb34d78f746af6cd
Devin Nguyen <nguyed5@rpi.edu>
2015-04-28 17:06:40 -0400

    bye bye oauth.js

--- oauth.js
+++
@@ -1,9 +0,0 @@
-var ids = {
-	facebook: {
-		clientID: '1419912294985026',
-		clientSecret: 'c11f906989dd9fd5aaf57fb3bb27d538',
-		callbackURL: 'http://localhost:5000/auth/facebook/callback'
-	}
-}
-
-module.exports = ids
